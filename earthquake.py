b9ee1325a06601d9fbaa00c698ee72dc46dbb9ba
gng0101 <Gordon@dyn-160-39-163-50.dyn.columbia.edu>
2014-06-09 13:04:04 -0400

    Learned a little bit about functions

--- earthquake.py
+++ earthquake.py
@@ -1,1 +1,18 @@
-print "Hello World"
+
+def greeting():
+	name = raw_input("What is your name? ")
+	print "Hello World " + name
+
+#greeting()
+
+def hello(name):
+	print "Hello, " + name
+#It connects "Soma" to name
+#hello("Soma")
+
+def add(a, b)":
+	return a + b
+	
+hello("Soma")
+
+print add(2,3) + add(4,5)
